1633fa8bc7cdf4866ee85096ca2c8170d06a2982
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-10-24 15:00:16 +0200

    react client and both services running

--- comments/index.js
+++ comments/index.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {};
+const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req,res) => {
-    res.send(posts)
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req,res) => {
-    console.log('A post!')
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
-    posts[id] = {
-        id, title
-    };
-    res.status(201).send(posts[id]);
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-    console.log('Listening on 4001')
+  console.log('Listening on 4001');
 });

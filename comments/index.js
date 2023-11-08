088d9a599449301356c84f5472a62b85bdba8a96
Juan Tena Gimeno <61567119+Juategi@users.noreply.github.com>
2023-11-08 19:12:16 +0100

    comment updated flow

--- comments/index.js
+++ comments/index.js
@@ -20,21 +20,33 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
     type : 'CommentCreated',
-    data: { id: commentId, content, postId: req.params.id }
+    data: { id: commentId, content, postId: req.params.id , status: 'pending'}
   });
 
   res.status(201).send(comments);
 });
 
 app.post('/events', async (req, res) => {
-  console.log("Recieved event", req.body.type );
-  res.send({});
+  const {type, data} = req.body;
+  if(type === 'CommentModerated'){
+    const {postId, id, status} = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    await axios.post('http://localhost:4005/events', {
+    type : 'CommentUpdated',
+    data: { id: id, content: comment.content, postId: postId , status: status}
+    });
+  }
+   res.status(201).send({});
 });
 
 app.listen(4001, () => {
